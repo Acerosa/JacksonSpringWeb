22f69d7e336897216f96875127345f1c75e9cdba
ricardorosa <rjrosa@outlook.com>
2023-11-13 13:26:05 +0000

    Committing using jackson to create json from java and vice versa

--- src/test/java/com/rr/JacksonSpringWeb/JacksonSpringWebApplicationTests.java
+++ src/test/java/com/rr/JacksonSpringWeb/JacksonSpringWebApplicationTests.java
@@ -1,13 +1,50 @@
 package com.rr.JacksonSpringWeb;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rr.JacksonSpringWeb.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JacksonSpringWebApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testThatObjectMapperCanCreateJSONFromObject() throws JsonProcessingException {
+		ObjectMapper ojectMapper = new ObjectMapper();
+		Book book = Book.builder()
+				.isbn("978-02411-894-50")
+				.title("Who Rules the World?")
+				.author("Noam Chomsky")
+				.yearPublished("2017")
+				.build();
+
+		String result = ojectMapper.writeValueAsString(book);
+		assertThat(result).isEqualTo("{\"isbn\":\"978-02411-894-50\",\"title\":\"Who Rules the World?\",\"author\":\"Noam Chomsky\",\"yearPublished\":\"2017\"}");
+	}
+
+	@Test
+	void testThatObjectMapperCanCreateJavaFromJSONObject() throws JsonProcessingException {
+
+		Book book = Book.builder()
+				.isbn("978-02411-894-50")
+				.title("Who Rules the World?")
+				.author("Noam Chomsky")
+				.yearPublished("2017")
+				.build();
+
+		String json = "{\"isbn\":\"978-02411-894-50\",\"title\":\"Who Rules the World?\",\"author\":\"Noam Chomsky\",\"yearPublished\":\"2017\"}";
+
+		ObjectMapper objectMapper = new ObjectMapper();
+		Book result = objectMapper.readValue(json, Book.class);
+
+		assertThat(result).isEqualTo(book);
 	}
 
 }
